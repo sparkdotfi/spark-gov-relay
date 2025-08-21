2e4b4dd6e2dd70373db47b290114b05100230c83
Tabish Shaikh <bonjour.tabish@gmail.com>
2025-08-21 22:46:15 +0530

    initial commit

---
+++ test/LZCrosschainTest.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import './CrosschainTestBase.sol';
+
+import { LZBridgeTesting } from 'lib/xchain-helpers/src/testing/bridges/LZBridgeTesting.sol';
+import { LZForwarder }     from 'lib/xchain-helpers/src/forwarders/LZForwarder.sol';
+import { LZReceiver }      from 'lib/xchain-helpers/src/receivers/LZReceiver.sol';
+
+import { LZCrosschainPayload } from './payloads/LZCrosschainPayload.sol';
+
+contract LZCrosschainTest is CrosschainTestBase {
+
+    using DomainHelpers   for *;
+    using LZBridgeTesting for *;
+
+    function deployCrosschainPayload(IPayload targetPayload, address bridgeReceiver)
+        internal override returns (IPayload)
+    {
+        return IPayload(new LZCrosschainPayload(
+            LZForwarder.ENDPOINT_ID_BNB,
+            LZForwarder.ENDPOINT_ETHEREUM,
+            bridgeReceiver,
+            targetPayload,
+            bridgeReceiver
+        ));
+    }
+
+    function setupDomain() internal override {
+        remote = getChain('bnb_smart_chain').createFork();
+        bridge = LZBridgeTesting.createLZBridge(
+            mainnet,
+            remote
+        );
+
+        remote.selectFork();
+        bridgeReceiver = address(new LZReceiver(
+            LZForwarder.ENDPOINT_BNB,
+            LZForwarder.ENDPOINT_ID_ETHEREUM,
+            bytes32(uint256(uint160(defaultL2BridgeExecutorArgs.ethereumGovernanceExecutor))),
+            vm.computeCreateAddress(address(this), 3)
+        ));
+
+        mainnet.selectFork();
+        vm.deal(L1_SPARK_PROXY, 0.01 ether);
+    }
+
+    function relayMessagesAcrossBridge() internal override {
+        bridge.relayMessagesToDestination(true, defaultL2BridgeExecutorArgs.ethereumGovernanceExecutor, bridgeReceiver);
+    }
+
+}
---
+++ test/payloads/LZCrosschainPayload.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.0;
+
+import { OptionsBuilder } from "layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
+
+import { LZForwarder, ILayerZeroEndpointV2 } from 'lib/xchain-helpers/src/forwarders/LZForwarder.sol';
+
+import { CrosschainPayload, IPayload } from './CrosschainPayload.sol';
+
+contract LZCrosschainPayload is CrosschainPayload {
+
+    using OptionsBuilder for bytes;
+
+    address public immutable lzEndpoint;
+    address public immutable receiver;
+
+    uint32 public immutable dstEid;
+
+    constructor(
+        uint32   _dstEid,
+        address  _lzEndpoint,
+        address  _receiver,
+        IPayload _targetPayload,
+        address  _bridgeReceiver
+    ) CrosschainPayload(_targetPayload, _bridgeReceiver) {
+        dstEid     = _dstEid;
+        lzEndpoint = _lzEndpoint;
+        receiver   = _receiver;
+    }
+
+    function execute() external override {
+        bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(200_000, 0);
+
+        LZForwarder.sendMessage(
+            dstEid,
+            bytes32(uint256(uint160(receiver))),
+            ILayerZeroEndpointV2(lzEndpoint),
+            encodeCrosschainExecutionMessage(),
+            options
+        );
+    }
+
+}
