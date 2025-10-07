7b46efdc57bd867b0b503305ad963f8c1def6b7b
Tabish Shaikh <bonjour.tabish@gmail.com>
2025-10-07 21:12:08 +0530

    save

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,6 +5,8 @@ import { console } from "forge-std/console.sol";
 
 import { Ethereum } from "lib/spark-address-registry/src/Ethereum.sol";
 
+import { LZForwarder } from "lib/xchain-helpers/src/forwarders/LZForwarder.sol";
+
 import { Script } from 'forge-std/Script.sol';
 
 import { Deploy } from "../deploy/Deploy.sol";
@@ -89,3 +91,29 @@ contract DeployUnichainExecutor is Script {
 
 }
 
+contract DeployAvalancheExecutor is Script {
+
+    function run() public {
+        vm.createSelectFork(getChain("avalanche").rpcUrl);
+
+        vm.startBroadcast();
+
+        address executor = Deploy.deployExecutor(0, 7 days);
+        address receiver = Deploy.deployLZReceiver({
+            destinationEndpoint : LZForwarder.ENDPOINT_AVALANCHE,
+            srcEid              : LZForwarder.ENDPOINT_ID_ETHEREUM,
+            sourceAuthority     : Ethereum.SPARK_PROXY,
+            executor            : executor,
+            delegate            : address(1),
+            owner               : address(1)
+        });
+
+        console.log("executor deployed at:", executor);
+        console.log("receiver deployed at:", receiver);
+
+        Deploy.setUpExecutorPermissions(executor, receiver, msg.sender);
+
+        vm.stopBroadcast();
+    }
+
+}
